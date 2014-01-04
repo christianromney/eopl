ed4b2cbcd33e11296a78e50afa243cecf5f39d11
Christian Romney <cromney@pointslope.com>
2014-01-04 15:17:13 -0500

    Run test suite against all custom environment implementations
    Imported each implementation under a different prefix
    and run the test suite against all of them in turn
    with the help of a simple macro.
    
    Signed-off-by: Christian Romney <cromney@pointslope.com>

--- test/ch02/env-tests.rkt
+++ test/ch02/env-tests.rkt
@@ -1,31 +1,48 @@
 #lang racket
 (require rackunit
          rackunit/text-ui
-         "../../src/ch02/envs/hash.rkt")
+         (prefix-in h/ "../../src/ch02/envs/hash.rkt")
+         (prefix-in c/ "../../src/ch02/envs/closure.rkt")
+         (prefix-in d/ "../../src/ch02/envs/datatype.rkt"))
 
-(define env-a
-  (extend-env
-   'x 10
-   (extend-env
-    'y 1
-    (extend-env
-     'x 1
-     (empty-env)))))
+;; Define the test suite
+(define env-test-suite
+  (lambda (apply-fn extend-fn empty-fn)
+    (let ([env-a (extend-fn
+                  'x 10
+                  (extend-fn
+                   'y 1
+                   (extend-fn
+                    'x 1
+                    (empty-fn))))])
 
-(run-tests
- (test-suite
-  "Environment lookup"
-  (test-equal? "Lookup rebound var"
-               (apply-env env-a 'x) 10)
+      (test-suite
+       "Environment lookup"
+       (test-equal? "Lookup rebound var"
+                    (apply-fn env-a 'x) 10)
 
-  (test-equal? "Lookup nested bound var"
-               (apply-env env-a 'y) 1)
+       (test-equal? "Lookup nested bound var"
+                    (apply-fn env-a 'y) 1)
 
-  (test-exn "Invalid environment" exn?
-            (lambda ()
-              (apply-env
-               (extend-env 'a 1 "I-am-an-invalid-env") 'b)))
+       (test-exn "Invalid environment" exn?
+                 (lambda ()
+                   (apply-fn
+                    (extend-fn 'a 1 "I-am-an-invalid-env") 'b)))
 
-  (test-exn "No binding found" exn?
-            (lambda ()
-              (apply-env env-a 'z)))))
+       (test-exn "No binding found" exn?
+                 (lambda ()
+                   (apply-fn env-a 'z)))))))
+
+
+;; Macro for running the test suite against multiple implementations
+(define-syntax run-for-all
+  (syntax-rules ()
+    [(_ suite ((a b c) ...))
+     (begin
+       (run-tests (suite a b c)) ...)]))
+
+;; Run all the tests
+(run-for-all env-test-suite
+             ((h/apply-env h/extend-env h/empty-env)
+              (c/apply-env c/extend-env c/empty-env)
+              (d/apply-env d/extend-env d/empty-env)))
