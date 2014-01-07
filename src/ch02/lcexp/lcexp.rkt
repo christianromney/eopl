49ed28a225938d3c7fb2a54c7bdc6405f3c2261c
Christian Romney <cromney@pointslope.com>
2014-01-06 21:42:55 -0500

    Exercise 2.15 Lambda Calculus interface representation
    Signed-off-by: Christian Romney <cromney@pointslope.com>

---
+++ src/ch02/lcexp/lcexp.rkt
@@ -0,0 +1,77 @@
+#lang eopl
+;; Lambda Calculus Representation
+;;
+;; Constructors
+;;
+;; var-exp: Var -> LcExp
+(define (var-exp var)
+  var)
+
+;; lambda-exp: Var x LcExp -> LcExp
+(define (lambda-exp var lcexp)
+  (list 'lambda (list var) lcexp))
+
+;; app-exp: LcExp x LcExp -> LcExp
+(define (app-exp rator rand)
+  (list rator rand))
+
+;; Predicates
+;;
+;; var-exp?: LcExp -> Bool
+(define (var-exp? lcexp)
+  (and (symbol? lcexp)
+       (not (eqv? lcexp 'lambda))))
+
+;; lambda-exp?: LcExp -> Bool
+(define (lambda-exp? lcexp)
+  (and (list? lcexp)
+       (= 3 (length lcexp))
+       (eqv? (car lcexp) 'lambda)))
+
+;; app-exp?: LcExp -> Bool
+(define (app-exp? lcexp)
+  (and (list? lcexp)
+       (not (lambda-exp? lcexp))
+       (= 2 (length lcexp))))
+
+;; Extractors
+;;
+;; var-exp->var: LcExp -> Var
+(define (var-exp->var lcexp)
+  lcexp)
+
+;; lambda-exp->bound-var: LcExp -> Var
+(define (lambda-exp->bound-var lcexp)
+  (caadr lcexp))
+
+;; lambda-exp->body: LcExp -> LcExp
+(define (lambda-exp->body lcexp)
+  (caddr lcexp))
+
+;; app-exp->rator: LcExp -> LcExp
+(define (app-exp->rator lcexp)
+  (car lcexp))
+
+;; app-exp->rand: LcExp -> LcExp
+(define (app-exp->rand lcexp)
+  (cadr lcexp))
+
+;; From book:
+;; occurs-free? : Sym × LcExp → Bool
+(define occurs-free?
+  (lambda (search-var exp)
+    (cond
+     [(var-exp? exp)
+      (eqv? search-var (var-exp->var exp))]
+     [(lambda-exp? exp)
+      (and
+       (not (eqv? search-var (lambda-exp->bound-var exp)))
+       (occurs-free? search-var (lambda-exp->body exp)))]
+     [else
+      (or
+       (occurs-free? search-var (app-exp->rator exp))
+       (occurs-free? search-var (app-exp->rand exp)))])))
+
+(provide var-exp? var-exp lambda-exp? lambda-exp app-exp? app-exp
+         var-exp->var app-exp->rator app-exp->rand
+         lambda-exp->body lambda-exp->bound-var occurs-free?)
---
+++ test/ch02/lcexp.rkt
@@ -0,0 +1,58 @@
+#lang racket
+(require rackunit
+         rackunit/text-ui
+         "../../src/ch02/lcexp/lcexp.rkt")
+
+(define lambda-calc-suite
+  (let ([lam (lambda-exp 'x (app-exp (var-exp 'even?)
+                                     (var-exp 2)))])
+    (test-suite
+     "Tests for the lambda calculus interface"
+
+     (test-true "Should recognize var-exp"
+                (var-exp? (var-exp 'a)))
+
+     (test-true "Should recognize lambda-exp"
+                (lambda-exp? (lambda-exp 'a (var-exp 'a))))
+
+     (test-true "Should recognize app-exp"
+                (app-exp?
+                 (app-exp
+                  (lambda-exp 'a (var-exp 'a))
+                  (var-exp 1))))
+
+     (test-equal? "Should extract bound var"
+                  (lambda-exp->bound-var lam) 'x)
+
+     (test-equal? "Should extract function from application"
+                  (var-exp->var (app-exp->rator (lambda-exp->body lam)))
+                  'even?)
+
+     (test-equal? "Should extract argument from application"
+                  (var-exp->var (app-exp->rand (lambda-exp->body lam)))
+                  2)
+
+     (test-true "Should recognize free variable as identifier"
+                (occurs-free? 'a (var-exp 'a)))
+
+     (test-true "Should recognize free variables in lambda expression"
+                (occurs-free? 'a
+                              (lambda-exp 'x
+                                          (app-exp (var-exp 'display)
+                                                   (var-exp 'a)))))
+
+     (test-false "Should recognize bound variables are not free"
+                 (occurs-free? 'a
+                               (lambda-exp 'a
+                                           (app-exp (var-exp 'display)
+                                                    (var-exp 'a)))))
+
+     (test-true "Should recognize free variables in application expression"
+                (occurs-free? 'display
+                              (lambda-exp 'x
+                                          (app-exp (var-exp 'display)
+                                                   (var-exp 'a)))))
+
+     )))
+
+(run-tests lambda-calc-suite)
