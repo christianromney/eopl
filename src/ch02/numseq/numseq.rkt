aac400edc5bfe785d27e1459b524f41d10b5d4c4
Christian Romney <cromney@pointslope.com>
2014-01-08 21:49:24 -0500

    Refactored internal methods
    Signed-off-by: Christian Romney <cromney@pointslope.com>

--- src/ch02/numseq/numseq.rkt
+++ src/ch02/numseq/numseq.rkt
@@ -1,5 +1,6 @@
 #lang eopl
 
+;; Private implementation functions
 
 (define (empty-seq)
   '())
@@ -10,11 +11,34 @@
 (define (next-list s)
   (caddr s))
 
-(define (add-to-list n l)
+(define (next-item-left s)
+  (car (prev-list s)))
+
+(define (next-item-right s)
+  (car (next-list s)))
+
+(define (shrink-list l)
+  (cdr l))
+
+(define (shrink-prev-list s)
+  (shrink-list (prev-list s)))
+
+(define (shrink-next-list s)
+  (shrink-list (next-list s)))
+
+(define (grow-list n l)
   (cons n l))
+
+(define (grow-next-list n s)
+  (grow-list n (next-list s)))
+
+(define (grow-prev-list n s)
+  (grow-list n (prev-list s)))
 
 (define (make-numseq n left right)
   (list n left right))
+
+;; Public interface
 
 (define (number->sequence n)
   (make-numseq n
@@ -28,33 +52,31 @@
   (if (at-left-end? s)
       (eopl:error 'move-to-left "Can't move left of left end")
       (make-numseq
-       (car (prev-list s))
-       (cdr (prev-list s))
-       (add-to-list
-        (current-element s)
-        (next-list s)))))
+       (next-item-left s)
+       (shrink-prev-list s)
+       (grow-next-list
+        (current-element s) s))))
 
 (define (move-to-right s)
   (if (at-right-end? s)
       (eopl:error 'move-to-right "Can't move right of right end")
       (make-numseq
-       (car (next-list s))
-       (add-to-list
-        (current-element s)
-        (prev-list s))
-       (cdr (next-list s)))))
+       (next-item-right s)
+       (grow-prev-list
+        (current-element s) s)
+       (shrink-next-list  s))))
 
 (define (insert-to-left n s)
   (make-numseq
    (current-element s)
-   (add-to-list n (prev-list s))
+   (grow-prev-list n s)
    (next-list s)))
 
 (define (insert-to-right n s)
   (make-numseq
    (current-element s)
    (prev-list s)
-   (add-to-list n (next-list s))))
+   (grow-next-list n s)))
 
 (define (at-left-end? s)
   (null? (prev-list s)))
